27fffb5a1ea21a800974fce17d9e6053233faa98
Łukasz Jaroszuk <krortop@gmail.com>
2026-01-28 12:23:44 +0100

    czytelniejszy code

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 function getMemoStart(nr) {
 
     datas = [];
+
     for (i = 1; i <= nr; i++) {
         key = "mem_" + i;
         let zero = {
@@ -11,11 +12,13 @@ function getMemoStart(nr) {
         datas[i] = zero
         localStorage.setItem(key, JSON.stringify(zero));
     }
+
     return datas;
 }
 
 function getAllMemo(nr) {
     mem = localStorage.getItem("mem_1");
+
     if (mem) {
         datas = [];
         for (i = 1; i <= nr; i++) {
@@ -25,12 +28,14 @@ function getAllMemo(nr) {
         }
         return datas;
     }
+
     return null;
 }
 
 function changeMemory(nr, name, count = 1) {
     key = "mem_" + nr
     mem = JSON.parse(localStorage.getItem(key), true);
+
     if (mem) {
         mem[name] += count;
         localStorage.setItem(key, JSON.stringify(mem));
@@ -41,16 +46,19 @@ function saveBadWords(word1, word2) {
     let word = [word1, word2];
     words = JSON.parse(localStorage.getItem("bad_words"));
     isInList = false;
+
     for (i = 0; i < words.length; i++) {
         if (words[i][0] == word1) {
             isInList = true;
             break;
         }
     }
+
     if (!isInList) {
         words.push(word);
         localStorage.setItem("bad_words", JSON.stringify(words));
         return true;
+
     } else {
         return false;
     }
